b73d61232038aee7ff77a1c6ba2c0f41e4d55f22
durumi99 <kms990615@naver.com>
2023-04-24 17:22:07 +0900

    1

--- assets/js/core.js
+++ assets/js/core.js
@@ -19231,16 +19231,11 @@ wysihtml5.views.Textarea = wysihtml5.views.View.extend(
 })();
  })(window, jQuery);
 ;(function ($) { $.fn.datepicker.language['en'] = {
-    days: ['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
     daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
     months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
     monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    // days: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
-    // daysShort: ['일', '월', '화', '수', '목', '금', '토'],
-    // daysMin: ['일', '월', '화', '수', '목', '금', '토'],
-    // months: ['1월','2월','3월','4월','5월','6월', '7월','8월','9월','10월','11월','12월'],
-    // monthsShort: ['1월','2월','3월','4월','5월','6월', '7월','8월','9월','10월','11월','12월'],
     today: 'Today',
     clear: 'Clear',
     dateFormat: 'yyyy/mm/dd',
